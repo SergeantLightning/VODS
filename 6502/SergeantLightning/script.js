3c258c1cfd2fbc4ff6a2270ac0f91584aa5d1abb
SergeantLightning <rotpserver@outlook.com>
2024-09-12 13:33:07 -0400

    Add Files For 6502 Emulator

---
+++ 6502/SergeantLightning/script.js
@@ -0,0 +1,14 @@
+// Registers
+
+var A = 0     // Accumulator
+var X = 0     // X Register
+var Y = 0     // Y Register
+var S = 255   // Stack Pointer
+
+function start() {
+	return "This function is currently in development";
+}
+
+function toggleRef() {
+	return "This function is currently in development";
+}
