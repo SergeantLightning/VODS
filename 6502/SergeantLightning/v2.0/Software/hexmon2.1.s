3c9b5644056bf6c21573f22e42124217942ba999
SergeantLightning <rotpserver@outlook.com>
2024-11-07 22:32:40 -0500

    Upload hexmon2.1 source code... I accidentally uploaded the binary instead

--- 6502/SergeantLightning/v2.0/Software/hexmon2.1.s
+++ 6502/SergeantLightning/v2.0/Software/hexmon2.1.s
@@ -1,89 +1,139 @@
-; IMPORTANT: THIS VERSION COMPILES WITH CA65, LINKER CONFIG IS PROVIDED IN SOFTWARE DIRECTORY
-; Filename: slvods6502.cfg
+KBIN	= $7FFE			; Keyboard input
+TTYOUT	= $7FFB			; Teletype output
+IN	= $80			; 128 byte input buffer in ZP
 
-.setcpu "65C02"
-
-TTYOUT  := $7FFB 	; Teletype output
-KBIN    := $7FFE 	; Keyboard Input
-
-.segment "ZP"
-
-L: .res 1			; Address low byte
-H: .res 1			; Address high byte
-WBH: .res 1			; High nibble of byte to write
-WBL: .res 1			; Low nibble of byte to write
-READCNT: .res 1		; Counter for block reads
-HOFS: .res 1		; Screen horizontal offset
-
-.segment "RAM"
-
-IN: .res $100	 	; 256 Byte Input Buffer
-
-.segment "CODE"
+AL	= $0			; Address low
+AH	= $1			; Address high
+BYTE	= $2			; Last parsed byte
+RDCNT	= $3			; Number of bytes that have been printed
 
 	.org $8000
 
 RESET:
-	CLD				; Clear decimal arithmetic mode.
-	SEI				; No interrupts
-	LDY #0			; Set up buffer index
+	CLD
+	SEI
+BUFFOVRN:
+	LDX #0			; Setup input buffer index
 NEXTOP:
-	STZ KBIN		; Clear keyboard input
-	STZ L
-	STZ H
-	STZ WBH
-	STZ WBL
-	STZ READCNT		; Clear block read counter
-	STZ HOFS		; Clear screen horizontal offset
-BOOTMSG:
 	LDA #':'		; Ready symbol
 	STA TTYOUT		; Output it
+	STZ KBIN
+	STZ BYTE
+	STZ RDCNT		; Clear ZP variables
 GETKEY:
-	LDA KBIN		; Key pressed?
-	BEQ GETKEY		; No, loop until ready
-	STA IN,Y		; Store to buffer
-	STA TTYOUT		; Output character
-	STZ KBIN		; Clear key
+	LDA KBIN
+	BEQ GETKEY		; Wait until a key has been pressed
 	CMP #13			; CR?
-	BEQ GO			; Yes, fall though if no
+	BEQ GO			; Yes
 	CMP #8			; Backspace?
 	BEQ BKSP		; Yes
-	BRA GETKEY		; Wait for next key
+	CMP #32			; Space?
+	BEQ BLANK		; Yes
+	CMP #48			; Invalid character?
+	BCC NOSTOR		; Yes, reset
+	STA IN,X		; Store to buffer
+	INX			; Increment buffer index
+BLANK:
+	STA TTYOUT		; Output character
+NOSTOR:
+	BMI BUFFOVRN		; Reset if more than 128 chars were typed
+NEXTKEY:
+	STZ KBIN
+	BRA GETKEY		; Get next key
 BKSP:
-	DEY
-	BRA GETKEY
+	DEX
+	STA TTYOUT
+	BMI BUFFOVRN		; Decremented to value larger than 127
+	BRA NEXTKEY
 GO:
+	STA IN,X
+	STA TTYOUT
+	LDX #0
+	PHX
+RLOOP:
+	LDA IN,X
+	CMP #13			; Done reading command?
+	BEQ DONEREAD		; Yes
+	CMP #'>'		; Block read?
+	BEQ SETBLOCK		; Yes
+	CMP #'<'		; Write?
+	BEQ GOWRITE		; Yes
+	CMP #'R'		; Run program?
+	BEQ GORUN		; Yes
+	JSR SHIFT		; Shift target address
+	JSR ATB			; Convert current character to binary digit
+	ORA AL
+	STA AL			; Add to target address
+	INX
+	BRA RLOOP
+SETBLOCK:
+	INX
+	JSR GETBYTE		; Get count and store it in BYTE
+	BRA DONEREAD
+GORUN:
+	JMP (AL)
+GOWRITE:
+	LDA BYTE
+	PHA			; Save BYTE
+WLOOP:
+	INX
+	LDA IN,X
+	CMP #13
+	BEQ DONEWRITE
+	JSR GETBYTE
+	LDA BYTE
+	STA (AL)
+	JSR INADDR
+	BRA WLOOP
+DONEWRITE:
+	PLA
+	STA BYTE
+	PLX
+	JMP NEXTOP
+DONEREAD:
 	LDY #0
-READINPUT:
-	LDA IN,Y		; Get inputted character
-	CMP #13			; CR?
-	BEQ NEXTOP
+	CPY BYTE
+	BEQ READSINGLE
+OUTLOOP:
+	CPY BYTE		; Done with block read?
+	BEQ DONEOUT		; Yes
+	LDA (AL)		; Load byte at target address
+	JSR PRBYTE		; Print byte in A register as ASCII
+	INY
+	JSR INADDR		; Increment read address
+	BRA OUTLOOP
+DONEOUT:
+	PLX
+	LDA #13
+	STA TTYOUT
+	STA TTYOUT		; New line
+	JMP NEXTOP
+READSINGLE:
+	INC BYTE
+	BRA OUTLOOP
 
 ; SUBROUTINES
 
 SHIFT:
 	PHX
-	LDX #0
+	LDX #4
 SLOOP:
-	CPX #4
+	ASL AL
+	ROL AH
+	DEX
 	BEQ SDONE
-	CLC
-	ASL L
-	ROL H
-	INX
 	BRA SLOOP
 SDONE:
 	PLX
 	RTS
 
 PARSEBYTE:
-	CMP #10				; 0-9?
+	CMP #10			; 0-9?
 	BCC PARSEDIG		; Yes, fall through if no
 	CLC
 	ADC #55
 	RTS
 PARSEDIG:
-	CLC
 	ADC #48
 	RTS
 
@@ -94,26 +144,65 @@ ATB:
 	SBC #48			; Convert ASCII digit to binary digit
 	RTS
 LETTER:
-	SEC
 	SBC #55			; ASCII to binary
 	RTS
 
 INADDR:
 	PHA
-	LDA L
+	LDA AL
 	CMP #$FF
 	BEQ INHB		; Need to increment high byte
 INLB:
-	INC L
+	INC AL
 	PLA
 	RTS
 INHB:
-	INC H
+	INC AH
 	BRA INLB
 
-.segment "VECS"
+
+PRBYTE:
+	PHA
+	LSR
+	LSR
+	LSR
+	LSR			; Move high nibble to low nibble spot
+	JSR PARSEBYTE		; Convert to ASCII
+	STA TTYOUT		; Output it
+	PLA
+	AND #$0F		; Get low nibble
+	JSR PARSEBYTE
+	STA TTYOUT		; Convert and output it
+	LDA #32
+	STA TTYOUT		; Output space
+	INC RDCNT
+	LDA RDCNT
+	CMP #8
+	BEQ PRCR
+	BRA PRDONE
+PRCR:
+	LDA #13
+	STA TTYOUT
+	STZ RDCNT
+PRDONE:
+	RTS
+
+GETBYTE:
+	LDA IN,X		; Get high nibble of count
+	JSR ATB			; Convert to binary
+	ASL
+	ASL
+	ASL
+	ASL			; Shift to high nibble of read count
+	STA BYTE		; Store it
+	INX
+	LDA IN,X		; Get low nibble
+	JSR ATB			; Convert to binary
+	ORA BYTE		; Combine with high nibble
+	STA BYTE		; Store to read count
+	RTS
 
 	.org $FFFA
-	.word $0
-	.word RESET
-	.word $0
+	.word $0		; NMI
+	.word RESET		; Reset vector
+	.word $0		; IRQ
